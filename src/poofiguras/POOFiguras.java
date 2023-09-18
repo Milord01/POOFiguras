1dc12d7ed30f8647f710f9a6bb686c132d631adf
Camilord01 <124930730+Milord01@users.noreply.github.com>
2023-09-18 14:45:43 -0500

    Carga inicial

---
+++ src/poofiguras/POOFiguras.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package poofiguras;
+
+/**
+ *
+ * @author MILO GAVIRIA
+ */
+public class POOFiguras {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        FigurasGeometricas form= new FigurasGeometricas();
+        form.setVisible(true);
+    }
+    
+}
